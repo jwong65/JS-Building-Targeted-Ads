4cee124e52aae7114b9e11b7d5a881fff1807f14
unknown <jason.wong6507@gmail.com>
2022-11-10 19:27:52 -0500

    Added the basic get UserMeal time with if statments

--- public/app.js
+++ public/app.js
@@ -10,6 +10,26 @@ function getUserTime(){
 }
 console.log(getUserTime())
 
+function userMealTime(){
+    const timeofday = getUserTime() //it retunrs now.getHours()
+
+    if (timeofday>20){
+        return 'Late-night snack'
+    }
+    else if (timeofday >16){
+        return 'Dinner'
+    }
+    else if (timeofday>11)
+    {
+        return 'Lunch'
+    }
+    else{
+        return 'Breakfast'
+    }
+}
+
+console.log(userMealTime())
+
 // helper functions
 // check time of day
 
